f9b449f6a04b0c97ab56b203f305071eeb5fffd4
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-07 09:28:33 +0100

    changed func names to operators

--- controllers/calc_controller.py
+++ controllers/calc_controller.py
@@ -4,7 +4,7 @@ from models.calculator import *
 
 
 @app.route("/add/<int:num1>/<int:num2>")
-def index(num1, num2):
+def add(num1, num2):
     return render_template(
         "index.html",
         title="Addition Calculator",
@@ -16,7 +16,7 @@ def index(num1, num2):
 
 
 @app.route("/subtract/<int:num1>/<int:num2>")
-def index2(num1, num2):
+def subtract(num1, num2):
     return render_template(
         "index.html",
         title="Subtraction Calculator",
@@ -28,7 +28,7 @@ def index2(num1, num2):
 
 
 @app.route("/multiply/<int:num1>/<int:num2>")
-def index3(num1, num2):
+def multiply(num1, num2):
     return render_template(
         "index.html",
         title="Multiplication Calculator",
@@ -40,7 +40,7 @@ def index3(num1, num2):
 
 
 @app.route("/divide/<int:num1>/<int:num2>")
-def index4(num1, num2):
+def divide(num1, num2):
     return render_template(
         "index.html",
         title="Division Calculator",
